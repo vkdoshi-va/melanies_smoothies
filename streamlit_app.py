e04b5e9e73684dbebc09f72b9d6e65cda5c861e9
vkdoshi-va <vdoshi88@gmail.com>
2026-07-16 23:52:54 -0400

    Update streamlit_app.py
    Added table for displaying data

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,5 @@ if ingredients_list:
 #new code to allow for APIs to work
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
