6922e99e81307faca1c593e2bb9442fbe0d08ad7
vkdoshi-va <vdoshi88@gmail.com>
2026-07-16 23:49:24 -0400

    Update streamlit_app.py
    added a json

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ if ingredients_list:
 #new code to allow for APIs to work
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
