e71ac23691ad412dabad70334535985efad47841
vkdoshi-va <vdoshi88@gmail.com>
2026-07-17 00:21:09 -0400

    Update streamlit_app.py
    Trying Pandas LOC functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("Fruit
 
 #convert the snowpark dataframe to a panda dataframe so we can use LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe (pd_df)
-st.stop()
+#st.dataframe (pd_df)
+#st.stop()
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)    
@@ -33,11 +33,14 @@ if ingredients_list:
     ingredients_string = ""
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + " Nutrition Information")
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-
+        
     #st.write (ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
