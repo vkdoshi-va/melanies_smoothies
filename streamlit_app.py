1a73e73875534bc07ee900676f589faa041b0bf1
vkdoshi-va <vdoshi88@gmail.com>
2026-07-16 23:40:35 -0400

    Update streamlit_app.py
    Name before order

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,14 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
 
+name_on_order = st.text_input('Name on Smoothie:')
+st.write("The name on your smoothie will be:", name_on_order)    
+
 ingredients_list = st.multiselect (
     'Choose up to 5 ingredients: '
     , my_dataframe
     , max_selections=5
     )
-
-name_on_order = st.text_input('Name on Smoothie:')
-st.write("The name on your smoothie will be:", name_on_order)    
 
 if ingredients_list:
     ingredients_string = ''
